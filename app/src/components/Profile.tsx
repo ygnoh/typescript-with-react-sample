651296b11a748d939b27de1298455636d847eb67
yonggoonoh <nohyg90@gmail.com>
2018-06-08 01:27:37 +0900

    profile component SFC로 변경

--- app/src/components/Profile.tsx
+++ app/src/components/Profile.tsx
@@ -5,25 +5,18 @@ interface Props {
   job: string;
 }
 
-// Props interface로 props를 받을 것이라고 알림
-class Profile extends React.Component<Props> {
-  render() {
-    const { name, job } = this.props;
-
-    return (
-      <div>
-        <h1>프로필</h1>
-        <div>
-          <b>이름: </b>
-          {name}
-        </div>
-        <div>
-          <b>직업</b>
-          {job}
-        </div>
-      </div>
-    );
-  }
-}
+const Profile = ({name, job}: Props) => (
+  <div>
+    <h1>프로필</h1>
+    <div>
+      <b>이름: </b>
+      {name}
+    </div>
+    <div>
+      <b>직업</b>
+      {job}
+    </div>
+  </div>
+);
 
 export default Profile
