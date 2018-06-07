c40d66e78abd59ace40990178d3e17cd61f7c071
yonggoonoh <nohyg90@gmail.com>
2018-06-08 02:08:38 +0900

    todolist 렌더링

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Counter from './components/Counter';
 import Profile from './components/Profile';
+import TodoList from './components/TodoList';
 
 class App extends React.Component {
   public render() {
@@ -11,6 +12,7 @@ class App extends React.Component {
           job="job"
         />
         <Counter />
+        <TodoList />
       </div>
     );
   }
